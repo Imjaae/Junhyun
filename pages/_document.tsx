6f9efb9c974a4c68fa7101cb93d98917e636ad59
Imjaae <dlawodud96@naver.com>
2023-05-30 01:10:13 +0900

    Fix styled-components SSR fix _document.tsx code by @Imjaae #3

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,32 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <link
-        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:&display=swap"
-        rel="stylesheet"
-      />
-      <Head />
-      <body style={{ margin: "0px" }}>
-        <Main />
-        <NextScript />
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-        <Script
-          type="text/javascript"
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&libraries=services,clusterer&autoload=false`}
-          strategy="beforeInteractive"
-        />
-      </body>
-    </Html>
-  );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }
